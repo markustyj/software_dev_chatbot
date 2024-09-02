7a00b453b44230d0f22431950af4641207318257
tang <yongjiantang@macl7ld123yg0.speedport.ip>
2024-09-02 23:55:41 +0200

    delete open ai key

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
     //OpenAIAPIKey: 'YOUR_API_KEY'
-    OpenAIAPIKey: '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'
+    OpenAIAPIKey: ''
 
 };
